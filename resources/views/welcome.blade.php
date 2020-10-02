fd7418486843b9af0c68cf7050bd068da8f8e03d
Rustem.Garaev <Tel52093547!@>
2020-10-02 11:46:32 +0300

    Начал редактировать страницу Welcom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,38 +15,30 @@
     <body class="bg-gray-100">
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="text-right p-8">
+                <div class="text-right p-8 bg-red-600 text-gray-100">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a  href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a class="mr-6" href="{{ route('login') }}">Войти</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">Зарегистрироваться</a>
                         @endif
                     @endauth
                 </div>
             @endif
             <div class="p-8 text-center">
                 <img class="w-1/6 rounded-lg shadow-xl mx-auto" src="\img\logo.jpg" alt="logo">
-                <h1 class="text-4xl font-hairline text-blue-600 mt-10"><i class="fas fa-stream mr-6"></i>Категории</h1>
+                <h1 class="text-4xl font-hairline text-red-600 mt-10"><i class="fas fa-stream mr-6"></i>Категории</h1>
             </div>
-                <i class="fas fa-camera"></i>
-{{--            <div class="content">--}}
-{{--                <div class="title m-b-md">--}}
-{{--                    Laravel--}}
-{{--                </div>--}}
+            <div class="flex mx-auto bg-red-600 px-20 py-10">
+                <div class="h-20 bg-gray-100 px-10 py-6 mx-6 flex-auto text-center">Название категории</div>
+                <div class="h-20 bg-gray-100 px-10 py-6 mx-6 flex-auto text-center">Название категории</div>
+                <div class="h-20 bg-gray-100 px-10 py-6 mx-6 flex-auto text-center">Название категории</div>
+                <div class="h-20 bg-gray-100 px-10 py-6 mx-6 flex-auto text-center">Название категории</div>
+                <div class="h-20 bg-gray-100 px-10 py-6 mx-6 flex-auto text-center">Название категории</div>
+            </div>
 
-{{--                <div class="links">--}}
-{{--                    <a href="https://laravel.com/docs">Docs</a>--}}
-{{--                    <a href="https://laracasts.com">Laracasts</a>--}}
-{{--                    <a href="https://laravel-news.com">News</a>--}}
-{{--                    <a href="https://blog.laravel.com">Blog</a>--}}
-{{--                    <a href="https://nova.laravel.com">Nova</a>--}}
-{{--                    <a href="https://forge.laravel.com">Forge</a>--}}
-{{--                    <a href="https://github.com/laravel/laravel">GitHub</a>--}}
-{{--                </div>--}}
-{{--            </div>--}}
         </div>
     </body>
 </html>
